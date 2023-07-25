c5c2fb629558db0938e7de50a5fb24f3455fe8b9
Grégoire Hébert <gregoire@les-tilleuls.coop>
2023-07-25 12:22:24 +0200

    fix: metadata

--- src/Metadata/Tests/Extractor/PropertyMetadataCompatibilityTest.php
+++ src/Metadata/Tests/Extractor/PropertyMetadataCompatibilityTest.php
@@ -72,7 +72,7 @@ final class PropertyMetadataCompatibilityTest extends TestCase
         ],
         'iris' => ['https://schema.org/totalPrice'],
         'genId' => true,
-        'uriTemplate' => false,
+        'uriTemplate' => '/sub-resource-get-collection',
     ];
 
     /**
